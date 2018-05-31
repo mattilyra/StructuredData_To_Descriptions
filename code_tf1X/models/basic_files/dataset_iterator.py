2e01b9fb6309cad0796b3367930789bc620b85e0
Matti Lyra <matti.lyra@comtravo.com>
2018-05-31 10:40:22 +0200

    Ported tf API from 0.x to 1.x

---
+++ code_tf1X/models/basic_files/dataset_iterator.py
@@ -0,0 +1,229 @@
+import random
+import nltk
+import numpy as np
+import pickle
+import sys
+import copy
+import os.path
+import tensorflow as tf
+from . vocab import *
+
+
+class Datatype:
+    def __init__(self, name,title,label, content, exm, max_length_content, max_length_title):
+        """ Defines the dataset for each category valid/train/test
+
+        Args:
+            name : Name given to this partition. For e.g. train/valid/test
+            title: The description of the input infobox.
+            label: Correct labels that will be used for loss computation
+            content: Input Infobox
+            exm:  Number of samples in this dataset split
+            max_length_content : Maximum length of input Infoboxes among all samples
+            max_length_title: Maximum length of description among all samples
+            
+            global_count_train: pointer to retrieve the next batch during training
+            global_count_test : pointer to retrieve the next batch during testing
+
+        Example for title, label:
+           Description: <s> The apple is red <eos>
+        title: <s> The apple is red
+        label: The apple is red <eos>
+        """
+
+        self.name = name
+        self.title = title
+        self.content = content
+        self.labels = label
+        self.number_of_examples = exm
+        self.max_length_content = max_length_content
+
+        # As said in the given example the length of the title
+        # will be one less than the length of the description
+        self.max_length_title = max_length_title - 1
+
+
+        self.global_count_train = 0
+        self.global_count_test  = 0
+
+
+class PadDataset:
+    def find_max_length(self, data, count, batch_size):
+
+        """ Finds the maximum sequence length for data of 
+            size batch_size
+
+            Args:
+                data: The data from which sequences will be chosen
+                count: The pointer from which retrieval should be done.
+                batch_size: Number of examples to be taken to find max.
+
+        """
+        data = data[count:count + batch_size]
+        return max(len(data[i]) for i,_ in enumerate(data))
+
+    def pad_data(self, data, max_length):
+
+        """ Pad the data to max_length given
+
+            Args: 
+                data: Data that needs to be padded
+                max_length : The length to be achieved with padding
+
+            Returns:
+                padded_data : Each sequence is padded to make it of length
+                              max_length.
+        """
+
+        padded_data = []
+
+        for lines in data:
+            if (len(lines) < max_length):
+                temp = np.lib.pad(lines, (0,max_length - len(lines)),
+                                  'constant', constant_values=0)
+            else:
+                temp = lines[:max_length]
+            padded_data.append(temp)
+
+        return padded_data
+
+    def make_batch(self, data, batch_size, idx, max_length):
+
+        """ Make a matrix of size [batch_size * max_length]
+            for given dataset
+
+            Args:
+                data: Make batch from this dataset
+                batch_size : batch size
+                idx : pointer from where retrieval will be done
+                max_length : maximum length to be padded into
+
+            Returns
+                batch: A matrix of size [batch_size * max_length]
+                count: The point from where the next retrieval is done.
+        """
+
+        batch = []
+        batch = data[idx:idx+batch_size]
+        idx = idx + batch_size
+
+        while (len(batch) < batch_size):
+            batch.append(np.zeros(max_length, dtype = int))
+            idx = 0
+
+        batch = self.pad_data(batch,max_length)
+
+        batch = np.transpose(batch)
+        return batch, idx
+
+    def next_batch(self, dt, batch_size, c=True):
+
+        # c= True(False): Corresponds to the batch will be used for training(testing), 
+        # Pick datapoints after the previously trained(tested) (batch)example.
+        # idx denotes the index in the dataset
+        if (c is True):
+            idx = dt.global_count_train
+        else:
+            idx = dt.global_count_test
+
+        max_length_content = self.datasets["train"].max_length_content
+        max_length_title   = self.datasets["train"].max_length_title
+
+        # idx_temp to account for the fact that the number of examples
+        # might not be a multiple of batch_size. We need to keep track of overflow
+        # from the number of datapoints.
+        contents, idx_temp = self.make_batch(dt.content, batch_size, idx, max_length_content)
+        titles, _ = self.make_batch(dt.title, batch_size, idx, max_length_title)
+        labels, _ = self.make_batch(dt.labels, batch_size, idx, max_length_title)
+
+        # titles contains the actual labels
+        # we make a copy of titles to ensure that we calculate
+        # loss for only those time steps which contain non pad symbols
+        weights = copy.deepcopy(titles)
+
+        for i in range(titles.shape[0]):
+            for j in range(titles.shape[1]):
+                # if weights[i][j] == 0 then the symbol is a pad
+                # and we do not want to compute loss for that time step
+                # else we compute the loss.
+                if (weights[i][j] > 0):
+                    weights[i][j] = 1
+                else:
+                    weights[i][j] = 0
+
+        if (c == True):
+            dt.global_count_train = idx_temp % dt.number_of_examples
+        else:
+            dt.global_count_test = idx_temp % dt.number_of_examples
+
+        return contents, titles, labels, weights, max_length_content, max_length_title
+
+    def load_data_file(self,name, title_file, content_file):
+
+        title = open(title_file,'rb')
+        content = open(content_file,'rb')
+
+        title_encoded = []
+        content_encoded = []
+        label_encoded = []
+
+        max_title = 0
+        for lines in title:
+
+            temp = [self.vocab.encode_word(word) for word in lines.split()]
+            if (len(temp) > max_title):
+                max_title = len(temp)
+            title_encoded.append(temp[:-1])
+            label_encoded.append(temp[1:])
+
+        max_content = 0
+        for lines in content:
+            temp = [self.vocab.encode_word(word) for word in lines.split()]
+            if (len(temp) > max_content):
+                max_content = len(temp)
+            content_encoded.append(temp)
+
+        return Datatype(name = name,
+                        title = title_encoded,
+                        label = label_encoded,
+                        content = content_encoded,
+                        exm = len(title_encoded),
+                        max_length_content = max_content,
+                        max_length_title = max_title)
+
+    def load_data(self, wd="../Data/"):
+
+        s = wd
+        self.datasets = {}
+        for i in ("train", "valid", "test"):
+            temp_t = s + i + "_summary"
+            temp_v = s + i + "_content"
+            self.datasets[i] = self.load_data_file(i, temp_t, temp_v)
+
+
+    def __init__(self,  working_dir = "../Data/", embedding_size=100, vocab_frequency = 73,
+                 embedding_dir = "../Data/", global_count = 0):
+
+        filenames = [working_dir + "train_summary" , working_dir + "train_content"]
+        #filenames = ["../DP_data/all_files"]
+
+        self.global_count = 0
+        self.vocab = Vocab()
+        self.vocab.construct_vocab(filenames, embedding_size, vocab_frequency, embedding_dir)
+        self.load_data(working_dir)
+
+    def length_vocab(self):
+        return self.vocab.len_vocab
+
+
+    def decode_to_sentence(self, decoder_states):
+        s = ""
+        for temp in (decoder_states):
+            if temp not in self.vocab.index_to_word:
+                word = "<unk>"
+            else:
+                word = self.vocab.decode_word(temp)
+
+            s = s + " " + word
+        return s
+
---
+++ code_tf1X/models/basic_files/vocab_hier.py
@@ -0,0 +1,301 @@
+import os.path
+import operator
+import pickle
+from nltk.tokenize import WhitespaceTokenizer
+from gensim.models import Word2Vec
+import gensim
+from collections import defaultdict
+from math import sqrt
+import numpy as np
+import sys
+
+class Vocab:
+    def __init__(self):
+
+        """ Initalize the class parameters to default values
+        """
+
+        self.word_to_index = {}
+        self.index_to_word = {}
+        self.unknown       = "<unk>"
+        self.end_of_sym    = "<eos>"
+        self.start_sym     = "<s>"
+        self.padding       = "<pad>"
+        self.word_freq     = {}
+        self.len_vocab     = 0
+        self.total_words   = 0
+        self.embeddings    = None
+
+
+    def get_global_embeddings(self, filenames, embedding_size, embedding_dir):
+
+        """ Construct the Embedding Matrix for the sentences in filenames.
+
+            Args:
+                filenames: File names of the training files.
+                embedding_size: Dimensions for the embedding to be used.
+
+            Returns
+                Embedding matrix.
+        """
+        sentences = []
+
+        if (os.path.exists(embedding_dir + 'vocab_len.pkl')):
+            with open(embedding_dir + "vocab_len.pkl", 'rb') as fh:
+                vocab_len_stored = pickle.load(fh)
+        else:
+            vocab_len_stored = 0
+
+        if vocab_len_stored == self.len_vocab and os.path.exists(embedding_dir + "embeddings.pkl"):
+            print("Load file")
+            with open(embedding_dir +  "embeddings.pkl", 'rb') as fh:
+                self.embeddings = pickle.load(fh)
+            return None
+
+        if os.path.exists(embedding_dir + 'embeddings'):
+            #model = gensim.models.KeyedVectors.load_word2vec_format('../Data/embeddings.bin', binary = True)
+            #model = Word2Vec.load_word2vec_format('../Data/embeddings.bin', binary = True)
+            model = gensim.models.KeyedVectors.load_word2vec_format(embedding_dir + 'embeddings', binary=False)
+            print("Pretrained Embedding Loaded")
+        else:
+            for file in filenames:
+                with open(file, 'rb') as f:
+                    for lines in f:
+                        words = [lines.split()]
+                        sentences.extend(words)
+
+            model = Word2Vec(sentences, size=embedding_size, min_count=0)
+            model.save(embedding_dir + "embeddings.bin")
+
+        self.embeddings_model = model
+        return model
+
+
+    def add_constant_tokens(self):
+
+        """ Adds the constant tokens in the dictionary.
+        """
+
+        self.word_to_index[self.padding]    = 0
+        self.word_to_index[self.unknown]    = 1
+
+
+    def add_word(self, word):
+
+        """ Adds the word to the dictionary if not already present.
+
+        Arguments:
+            * word : Word to be added.
+
+        Returns:
+            * void
+        """
+        if word in self.word_to_index:
+            self.word_freq[word] = self.word_freq[word] + 1
+
+        elif word == "<pad>":
+            return
+        else:
+            index = len(self.word_to_index)
+            self.word_to_index[word] = index
+            self.word_freq[word] = 1
+
+    def create_reverse_dictionary(self):
+
+        """ Creates a mapping from index to the words
+            This will be used during the time of decoding the
+            indices to words.
+        """
+
+        for key, val in self.word_to_index.items():
+            self.index_to_word[val] = key
+
+    def construct_dictionary_single_file(self, filename):
+
+        """ Creates the dictionary from a single file.
+
+            Arguments:
+                * filename: The file from which dictionary
+                          will be constructed
+
+            Returns:
+                * void
+        """
+        with open(filename, 'rb') as f:
+            for lines in f:
+                for words in lines.split():
+                    self.add_word(words)
+
+
+    def fix_the_frequency(self, limit=0):
+
+        temp_word_to_index = {}
+        temp_index_to_word = {}
+
+        #get the list of the frequent words, upto the given limit.
+        word_list = []
+        count = 0
+
+        new_index = 2
+        for key in self.word_to_index:
+            if (self.word_freq[key] > limit):
+                temp_word_to_index[key] = new_index
+                temp_index_to_word[new_index] = key
+                new_index  = new_index + 1
+
+        print (new_index)
+        self.word_to_index = temp_word_to_index
+
+
+    def construct_dictionary_multiple_files(self, filenames):
+
+        """ Dictionary is made from all the files listed.
+
+            Arguments :
+                * filenames = List of the filenames
+
+            Returns :
+                * None
+        """
+
+        for files in filenames:
+            self.construct_dictionary_single_file(files)
+
+    def encode_word(self, word):
+
+        """ Conver the word to the particular index
+
+            Arguments :
+                * word: Given word is converted to index.
+
+            Returns:
+                * index of the word
+        """
+        if word not in self.word_to_index:
+            word = self.unknown
+        return self.word_to_index[word]
+
+    def decode_word(self, index):
+
+        """ Index is converted to its corresponding word.
+
+            Argument:
+                * index: The index to be encoded.
+
+            Returns:
+                * returns the corresponding word
+        """
+        if index not in self.index_to_word:
+            return self.unknown
+        return self.index_to_word[index]
+
+
+    def get_embeddings_pretrained(self, embedding_size, embedding_dir):
+
+        """ Embeddings are appened based on the index of the
+        word in the matrix self.embeddings.
+        """
+
+        sorted_list = sorted(list(self.index_to_word.items()), key = operator.itemgetter(0))
+        np.random.seed(1357)
+
+
+        if (os.path.exists(embedding_dir + 'vocab_len.pkl')):
+            vocab_len_stored = pickle.load(open(embedding_dir + "vocab_len.pkl"))
+        else:
+            vocab_len_stored = 0
+
+        if vocab_len_stored == self.len_vocab and os.path.exists(embedding_dir + "embeddings.pkl"):
+            self.embeddings = pickle.load(open(embedding_dir + "embeddings.pkl"))
+            return
+
+        embeddings = []
+        count = 0
+        for index, word in sorted_list:
+            try:
+                self.embeddings_model
+
+                if word in self.embeddings_model.vocab:
+                    count = count + 1
+                    embeddings.append(self.embeddings_model[word])
+                else:
+                    if word in ['<pad>', '<s>', '<eos>']:
+                        temp = np.zeros((embedding_size))
+
+                    else:
+                        temp = np.random.uniform(-sqrt(3)/sqrt(embedding_size), sqrt(3)/sqrt(embedding_size), (embedding_size))
+
+                    embeddings.append(temp)
+
+            except:
+                if word in ['<pad>', '<s>', '<eos>']:
+                    temp = np.zeros((embedding_size))
+                else:
+                    temp = np.random.uniform(-sqrt(3)/sqrt(embedding_size), sqrt(3)/sqrt(embedding_size), (embedding_size))
+
+                embeddings.append(temp)
+
+        print(("Number of words in the count" , count))
+        self.embeddings = np.asarray(embeddings)
+        self.embeddings = self.embeddings.astype(np.float32)
+
+        pickle.dump(self.embeddings, open(embedding_dir + "embeddings.pkl", "wb"))
+        pickle.dump(self.len_vocab, open(embedding_dir + "vocab_len.pkl", "wb"))
+
+
+    def construct_vocab(self, filenames, embedding_size=100, vocab_frequency=74, embedding_dir="../Data/"):
+
+
+        """ Constructs the embeddings and  vocabs from the parameters given.
+
+            Args:
+                * filenames: List of filenames to consider to make the vocab
+                * embeddings: The size of the embeddings to be considered.
+
+            Returns:
+                * void
+        """
+
+        self.construct_dictionary_multiple_files(filenames)
+        self.fix_the_frequency(vocab_frequency)
+        #self.remove_the_unfrequent(10000)
+        print(("Length of the dictionary is " + str(len(self.word_to_index))))
+        sys.stdout.flush()
+        self.add_constant_tokens()
+        self.create_reverse_dictionary()
+        self.get_global_embeddings(filenames, embedding_size, embedding_dir)
+        self.get_embeddings_pretrained(embedding_size, embedding_dir)
+
+        self.len_vocab = len(self.word_to_index)
+        print(("Length of the dictionary is " + str(len(self.word_to_index))))
+        self.total_words = float(sum(self.word_freq.values()))
+
+
+    def plot_the_frequencies(self):
+
+        x = list(self.index_to_word.keys())
+        y = []
+
+        for i in list(self.index_to_word.values()):
+            if i not in self.word_freq:
+                y.append(0)
+            else:
+                y.append(self.word_freq[i])
+
+
+        plt.hist(x, weights=y)
+        plt.show()
+
+def main():
+    x = Vocab()
+    filenames = ["../Gigaword/train_title", "../Gigaword/train_content"]
+    x.construct_vocab(filenames)
+    #x.plot_the_frequencies()
+
+    "The vocab is "
+    #for i in x.word_to_index:
+    #    print (i, x.word_freq[i])
+
+
+if __name__ == '__main__':
+    main()
